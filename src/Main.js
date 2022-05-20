811e7edd22b21d844f5bf670257f67354b93d8a0
Tanesha Brester <taneshabrester@gmail.com>
2022-05-20 16:01:49 -0700

    lab06 edit again

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import Form from "react-bootstrap/Form";
+import axios from "axios";
+
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      latitude: '',
+      longitude: '',
+    }
+  }
+  cityLocation = async (event) => {
+    event.preventDefault();
+    try{
+      // build out URl for locationIQ, add key from .env file, add cityName (this.state.cityName) all goes in URL
+      // await axios.get() then pass in URL
+      
+    } catch(error){
+      // put in error information, display errror through seprate component
+    }
+  }
+
+
   render() {
     return (
-      <div className=  "Main" >
+      <div className="Main" >
         <h2>City Explorer</h2>
-        <Form>
-        <Form.Group>
-          <Form.Label> Where would you like to explore?</Form.Label>
-          <Form.Control type='text' placeholder='seattle'/>
-        </Form.Group>
+        {/* basically this is what happens when you click the "Explore!" button */}
+        <Form onSubmit={this.cityLocation}>
+          <Form.Group>
+            <Form.Label> Where would you like to explore?</Form.Label>
+            {/* form control is blank box someone will type in, to grab that info you must listen with onChange, then setState with the value*/}
+            <Form.Control onChange={(event) => this.setState({ cityName: event.target.value })} type='text' placeholder='seattle' />
+          </Form.Group>
         </Form>
       </div>
     );
